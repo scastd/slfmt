f0b09561f5cbf6037ced54c8965fe1cbbdcc2897
scastd <scastd00@gmail.com>
2023-06-02 18:06:51 +0200

    ✨ Added all methods of `logger.h`. Support for importing the library

--- include/slfmt/logger.h
+++ include/slfmt/logger.h
@@ -2,11 +2,138 @@
 #define SLFMT_LOGGER_H
 
 #include <string>
+#include <memory>
+#include <fmt/format.h>
+#include "fmt/format.h"
+#include "color.h"
 
 namespace slfmt {
-	inline auto test(std::string param) -> std::string {
-		return param;
-	}
+    class Logger {
+    private:
+        std::string clazz_;
+
+        Logger() = default;
+
+        explicit Logger(std::string_view clazz) : clazz_(clazz) {}
+
+    public:
+        Logger(const Logger &) = delete;
+
+        Logger(Logger &&) = delete;
+
+        Logger &operator=(const Logger &) = delete;
+
+        Logger &operator=(Logger &&) = delete;
+
+        ~Logger() = default;
+
+        static std::unique_ptr<Logger> GetLogger(std::string_view c) {
+            return std::unique_ptr<Logger>(new Logger(c));
+        };
+
+        /*-------------------------*/
+        /* TRACE                   */
+        /*-------------------------*/
+
+        void Trace(std::string_view msg) const {
+            fmt::print(slfmt::color::TRACE_COLOR, "{}: {}\n", clazz_, msg);
+        }
+
+        template<typename... Args>
+        void Trace(std::string_view format, Args... args) const {
+            Trace(fmt::format(format, args...));
+        }
+
+        void Trace(std::string_view msg, const std::exception &e) const {
+            Trace(fmt::format(msg, e.what()));
+        }
+
+        /*-------------------------*/
+        /* DEBUG                   */
+        /*-------------------------*/
+
+        void Debug(std::string_view msg) const {
+            fmt::print(slfmt::color::DEBUG_COLOR, "{}: {}\n", clazz_, msg);
+        }
+
+        template<typename... Args>
+        void Debug(std::string_view format, Args... args) const {
+            Debug(fmt::format(format, args...));
+        }
+
+        void Debug(std::string_view msg, const std::exception &e) const {
+            Debug(fmt::format(msg, e.what()));
+        }
+
+        /*-------------------------*/
+        /* INFO                    */
+        /*-------------------------*/
+
+        void Info(std::string_view msg) const {
+            fmt::print(slfmt::color::INFO_COLOR, "{}: {}\n", clazz_, msg);
+        }
+
+        template<typename... Args>
+        void Info(std::string_view format, Args... args) const {
+            Info(fmt::format(format, args...));
+        }
+
+        void Info(std::string_view msg, const std::exception &e) const {
+            Info(fmt::format(msg, e.what()));
+        }
+
+
+        /*-------------------------*/
+        /* WARNING				   */
+        /*-------------------------*/
+
+        void Warn(std::string_view msg) const {
+            fmt::print(slfmt::color::WARN_COLOR, "{}: {}\n", clazz_, msg);
+        }
+
+        template<typename... Args>
+        void Warn(std::string_view format, Args... args) const {
+            Warn(fmt::format(format, args...));
+        }
+
+        void Warn(std::string_view msg, const std::exception &e) const {
+            Warn(fmt::format(msg, e.what()));
+        }
+
+        /*-------------------------*/
+        /* ERROR                   */
+        /*-------------------------*/
+
+        void Error(std::string_view msg) const {
+            fmt::print(slfmt::color::ERROR_COLOR, "{}: {}\n", clazz_, msg);
+        }
+
+        template<typename... Args>
+        void Error(std::string_view format, Args... args) const {
+            Error(fmt::format(format, args...));
+        }
+
+        void Error(std::string_view msg, const std::exception &e) const {
+            Error(fmt::format(msg, e.what()));
+        }
+
+        /*-------------------------*/
+        /* FATAL                   */
+        /*-------------------------*/
+
+        void Fatal(std::string_view msg) const {
+            fmt::print(slfmt::color::FATAL_COLOR, "{}: {}\n", clazz_, msg);
+        }
+
+        template<typename... Args>
+        void Fatal(std::string_view format, Args... args) const {
+            Fatal(fmt::format(format, args...));
+        }
+
+        void Fatal(std::string_view msg, const std::exception &e) const {
+            Fatal(fmt::format(msg, e.what()));
+        }
+    };
 }  // namespace slfmt
 
 #endif // SLFMT_LOGGER_H
--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,12 @@
 #include <slfmt.h>
 
 int main(int, char *[]) {
-	fmt::print(slfmt::color::magenta, "Hello, {}!\n", "world");
+    const auto logger = slfmt::Logger::GetLogger("main");
+    logger->Trace("Hello, {}!", "world");
+    logger->Debug("Hello, {}!", "world");
+    logger->Info("Hello, {}!", "world");
+    logger->Warn("Hello, {}!", "world");
+    logger->Error("Hello, {}!", "world");
+    logger->Fatal("Hello, {}!", "world");
 	return 0;
 }
