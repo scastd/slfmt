f20732795293b389f23d24d60d02dc913f1921e6
scastd <scastd00@gmail.com>
2023-06-04 17:44:49 +0200

    🧪 Removed non-compiling test

--- test/test.cpp
+++ test/test.cpp
@@ -4,8 +4,3 @@
 TEST_CASE("test version") {
 	REQUIRE(SLFMT_VERSION_STRING == std::string("0.1.0"));
 }
-
-TEST_CASE("test_example") {
-	std::string value = slfmt::test("hello!");
-	REQUIRE(value == std::string("hello!"));
-}
