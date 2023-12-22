0494291c79d712c2aae1ff90963635c01e5831c5
scastd <scastd00@gmail.com>
2023-12-22 10:46:28 +0100

    Update logging in basic_usage.cpp example

--- examples/basic_usage.cpp
+++ examples/basic_usage.cpp
@@ -22,18 +22,22 @@ private:
 
 public:
     void Test(std::string_view s) const {
-        s_logger->Info("{}", slfmt::LevelToString(slfmt::Level::INFO));
-        s_logger->Info("{}", "Test 1");
-        s_logger->Debug("{}", "Test 2");
-        s_logger->Trace("{}", "Test 3");
-        s_logger->Warn("{}, {}", "Test 4", s);
+        s_logger->Log(slfmt::Level::INFO, "{}", slfmt::LevelToString(slfmt::Level::INFO));
+        s_logger->Log(slfmt::Level::INFO, "{}", "Test 1");
+        s_logger->Log(slfmt::Level::DEBUG, "{}", "Test 2");
+        s_logger->Log(slfmt::Level::TRACE, "{}", "Test 3");
+        s_logger->Log(slfmt::Level::WARN, "{}, {}", "Test 4", s);
+        s_logger->Log(slfmt::Level::ERROR, "{}", "Test 5");
+        s_logger->Log(slfmt::Level::FATAL, "{}", "Test 6");
     }
 };
 
 int main(int argc, char *argv[]) {
     const TestClass test1;
     const TestClass2 test2;
+
     test1.Test("Example parameter");
     test2.Test("Second instance");
+
     return 0;
 }
